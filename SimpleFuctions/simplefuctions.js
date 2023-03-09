1b92f39d56336ab9cf25685f937135c681f8d509
WinPyro <niyazshakirov4@gmail.com>
2023-03-09 13:51:10 +0600

    Simplefuncstions

---
+++ SimpleFuctions/simplefuctions.js
@@ -0,0 +1,30 @@
+function returnNumber(num) {
+    return num;
+} 
+console.log(returnNumber(5))
+
+function returnString(String){
+    return String;
+}
+console.log(returnString("AidaiIsSimp"))
+
+function returnTrue(){
+    return true;
+}
+
+function returnFalse(){
+    return false;
+}
+
+function returnArray(array){
+    return array
+}
+console.log(array["2"] !== array["02"]);
+
+function HowMuchBottles(pepsi,totalnumber){
+    var obj = {
+        bottle: pepsi,
+        totalnumber: 5,
+    };
+    return obj;
+}
--- operators/operators.js
+++ operators/operators.js
@@ -1,6 +1,15 @@
+
+
+
+// Declare a variable "a" and it should be the number equal to 654321 plus 123456
 var a = 654321 + 123456;
+// Declare a variable "b" and it should be the number equal to 654321 minus 123456
 var b = 654321 - 123456;
+// Declare a variable "c" and it should be the number equal to 654321 times 123456
 var c = 654321 * 123456;
+// Declare a variable "d" and it should be the number equal to 654321 divided by 123456
 var d = 654321 / 123456;
+// Declare a variable "e" and it should be the number equal to 654321 modulus 123456
 var e = 654321 % 123456;
+// Declare a variable "f" and it should be the number equal to 2 to the 20th power
 var f = 2 ** 20;
