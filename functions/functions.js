f98c77512b56b0b89e1ef676a466e7a9d11a67ec
WinPyro <niyazshakirov4@gmail.com>
2023-03-13 12:01:52 +0600

    my work here is done

---
+++ functions/functions.js
@@ -0,0 +1,30 @@
+//Instructions: Make a function named "add" that takes two arguments, both numbers. It should return the sum of those numbers.
+const add=(num1 , num2)=> num1+num2; 
+
+//Instructions: Make a function named "subtract" that takes two arguments, both numbers. It should return the first number minus the second.
+
+const subtract=(num1 , num2)=> num1-num2;
+
+//Instructions: Make a function named "multiply" that takes two arguments, both numbers. It should return the numbers multiplied together.
+
+const multiply=(num1 , num2)=> num1*num2;
+
+//Instructions: Make a function named "divide" that takes two arguments, both numbers. It should return the first number divided by the second.
+
+const divide=(num1 , num2)=> num1/num2;
+
+//Instructions: Make a function named"exponent" that takes two arguments, both numbers. It should return thefirst number to the power of the second.
+
+const exponent=(num1 , num2)=> num1**num2;
+
+//Instructions: Make a function named"combineStrings" that takes two arguments, both strings. It shouldreturn a string that is both arguments combined.
+
+const combineStrings=(text1 , text2)=> `${num1}${num2}`;
+
+//Instructions: Make a function named "combineArrays" that takes two arguments, both arrays. It should return an array with the contents of the first, then second array within it.
+
+const combineArrays=(array1, array2)=> [...array1,...array2];
+
+//Instructions: Make a function named "combineObjects" that takes two arguments, both objects. It should return an object with the key/value pairs of both arguments. (Look up "spread operator" or "Object.assign()")
+
+const combineObjects=({name:Niyaz, age:19});
